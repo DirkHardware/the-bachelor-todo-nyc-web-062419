e798e204c2c1e6dcea0c8794dd13cd6914061a4a
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-26 15:42:23 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,16 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  contestants = []
+  contestants = data[season]
+  i = 0
+  while i < contestants.length
+    if contestants[i]["status"] == "Winner"
+      winner_name = contestants[i]["name"]
+      # winner_name = winner_name.split
+    else
+      i += 1
+    end
+  end
+  # winner_name[0]
 end
 
 def get_contestant_name(data, occupation)
